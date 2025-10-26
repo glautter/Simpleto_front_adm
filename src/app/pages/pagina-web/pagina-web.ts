debb637bb64a80d00d4d313cdbc795ad26b80f4d
Walmir Oliveira de Souza <walmir.souza.ext@unimednacional.coop.br>
2025-10-26 16:01:49 -0300

    Utilização do Header como componente

--- src/app/pages/pagina-web/pagina-web.ts
+++ src/app/pages/pagina-web/pagina-web.ts
@@ -1,28 +1,53 @@
-import { Component, ViewChild } from '@angular/core';
+// pagina-web.component.ts
+import { Component, ViewChild, AfterViewInit, Output, EventEmitter } from '@angular/core';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { BreakpointObserver } from '@angular/cdk/layout';
+import { HeaderComponent } from '../../layout/header/header';
+import { SidenavComponent } from "../../layout/sidenav/sidenav";
+
 @Component({
   selector: 'app-pagina-web',
   standalone: true,
+  templateUrl: './pagina-web.html',
+  styleUrls: ['./pagina-web.scss'], // <-- Corrigido: era "styleUrl"
   imports: [
     MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatListModule,
-  ],
-  templateUrl: './pagina-web.html',
-  styleUrl: './pagina-web.scss'
+    HeaderComponent,
+    SidenavComponent
+  ]
 })
-export class PaginaWebComponent {
+export class PaginaWebComponent implements AfterViewInit {
+  @Output() menuToggle = new EventEmitter<void>();
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
+
   isSidenavOpen = true;
   isMobile = false;
 
-  constructor(private observer: BreakpointObserver) {}
+  constructor(private observer: BreakpointObserver) { }
+
+  ngAfterViewInit(): void {
+    // Exemplo simples para detectar se é mobile e ajustar o comportamento
+    this.observer.observe(['(max-width: 800px)']).subscribe(result => {
+      this.isMobile = result.matches;
+      if (this.isMobile) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
+  onToggleSidenav() {
+    this.menuToggle.emit();
+  }
 }
