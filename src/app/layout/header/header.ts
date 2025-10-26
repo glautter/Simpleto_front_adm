f94627c17094e33838f5f139768d6dd3b1860072
Glautter Neves <glautterg@gmail.com>
2025-10-26 16:04:57 -0300

    retira unusing

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -3,14 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavContainer, MatSidenav, MatSidenavContent } from "@angular/material/sidenav";
-import { SidenavComponent } from "../sidenav/sidenav";
-import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavContainer, MatSidenav, SidenavComponent, MatSidenavContent, RouterOutlet],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './header.html',
   styleUrls: ['./header.scss']
 })
